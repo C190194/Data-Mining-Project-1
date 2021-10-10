64383131f6817b7e00f34a77ea891639288936d4
cswbibibi <caoshuwen1017@gmail.com>
2021-10-10 11:07:05 +0800

    ruleitem code cleaning

--- code/ruleitem.py
+++ code/ruleitem.py
@@ -9,78 +9,96 @@ Reference: https://www.cs.uic.edu/~hxiao/courses/cs594-slides.pdf
 
 
 class RuleItem:
-    """
-    cond_set: a dict with following fashion:
-            {item name: value, item name: value, ...}
-        e.g.
-            {A: 1, B: 1} (A, B are name of columns, here called "item", and in our code should be numerical index
-                          but not string)
-    class_label: just to identify the class it belongs to.
-    dataset: a list returned by read method. (see read.py)
-    cond_sup_count, rule_sup_count, support and confidence are number.
-    """
     def __init__(self, cond_set, class_label, dataset):
-        self.cond_set = cond_set
-        self.class_label = class_label
-        self.cond_sup_count, self.rule_sup_count = self._get_sup_count(dataset)
-        self.support = self._get_support(len(dataset))
-        self.confidence = self._get_confidence()
+        """ According to the paper, each frequent k-ruleitems consists of the following:
+        condset: a dictiondary that has key-value pair {"item name: value, item name: value...},
+        where the item name are name of the column attributes.
+        condsupCount: the support count of condset
+        rulesupCount: the support count of the ruleitem
+        y : the class label """
+        self.condset = condset
+        self.label = label
+        self.condsupCount, self.rulesupCount = self.calculate_supCount(data_list)
+        self.support = self.calculate_support(data_list)
+        self.confidence = self.calculate_confidence()
 
-    # calculate condsupCount and rulesupCount
-    def _get_sup_count(self, dataset):
-        cond_sup_count = 0
-        rule_sup_count = 0
-        for case in dataset:
-            is_contained = True
-            for index in self.cond_set:
-                if self.cond_set[index] != case[index]:
-                    is_contained = False
+
+    def calculate_supCount(self, data_list):
+        """ Count the condsupCount and rulesupCount respectively. """
+        # Initialization
+        condsupCount = 0
+        rulesupCount = 0
+        # iterate through every data_list
+        for data in data_list:
+            # Initialization check condition to be true
+            covered = True
+            # inner interation 
+            for index in self.condset:
+                # if the value of condset and data at the same index position is different
+                if self.condset[index] != data[index]:
+                    # there is nothing to count
+                    covered = False
                     break
-            if is_contained:
-                cond_sup_count += 1
-                if self.class_label == case[-1]:
-                    rule_sup_count += 1
-        return cond_sup_count, rule_sup_count
+            # if the value are the same at the same index position
+            if covered:
+                # value of condsupCount + 1
+                condsupCount += 1
+                # if they belong to the same label class
+                if self.label == data[-1]:
+                    # rulesupCpunt + 1
+                    rulesupCount += 1
+        return condsupCount, rulesupCount
 
-    # calculate support count
-    def _get_support(self, dataset_size):
-        return self.rule_sup_count / dataset_size
 
-    # calculate confidence
-    def _get_confidence(self):
-        if self.cond_sup_count != 0:
-            return self.rule_sup_count / self.cond_sup_count
-        else:
+    def calculate_support(self, data_list):
+        """ Calculate the support. """
+        # find the total data size
+        data_list_size = len(data_list)
+        support = self.rulesupCount / data_list_size
+        return support
+
+
+    def calculate_confidence(self):
+        """ Calculate the confidence. """
+        if self.condsupCount == 0:
             return 0
+        else:
+            # by formula
+            return self.rulesupCount / self.condsupCount
 
-    # print out the ruleitem
-    def print(self):
-        cond_set_output = ''
-        for item in self.cond_set:
-            cond_set_output += '(' + str(item) + ', ' + str(self.cond_set[item]) + '), '
-        cond_set_output = cond_set_output[:-2]
-        print('<({' + cond_set_output + '}, ' + str(self.cond_sup_count) + '), (' +
-              '(class, ' + str(self.class_label) + '), ' + str(self.rule_sup_count) + ')>')
 
-    # print out rule
+    def print_ruleitem(self):
+        """ Function to print out the rule item. """ 
+        condset_content = ''
+        for kv_pair in self.condset:
+            condset_content += '(' + str(kv_pair) + ', ' + str(self.condset[kv_pair]) + '), '
+        # remove the extra 2 characters, which is the ', '
+        condset_content = condset_content[:-2]
+        print('<({' + condset_content + '}, ' + 'condsupCount:' + str(self.condsupCount) + '), (' +
+              '(Class label: ' + str(self.label) + '), ' + 'sulesupCount:' + str(self.rulesupCount) + ')>')
+
+
     def print_rule(self):
-        cond_set_output = ''
-        for item in self.cond_set:
-            cond_set_output += '(' + str(item) + ', ' + str(self.cond_set[item]) + '), '
-        cond_set_output = '{' + cond_set_output[:-2] + '}'
-        print(cond_set_output + ' -> (class, ' + str(self.class_label) + ')')
+        """ Function to print out the rule. """ 
+        condset_content = ''
+        for kv_pair in self.condset:
+            condset_content += '(' + str(kv_pair) + ', ' + str(self.condset[kv_pair]) + '), '
+        # remove the extra 2 characters, which is the ', '
+        condset_content = '{' + condset_content[:-2] + '}'
+        # condset -> y
+        print(condset_content + ' -> (Class label: ' + str(self.label) + ')')
 
 
 # just for test
 if __name__ == '__main__':
-    cond_set = {0: 1, 1: 1}
-    class_label = 1
-    dataset = [[1, 1, 1], [1, 1, 1], [1, 2, 1], [2, 2, 1], [2, 2, 1],
+    condset = {0: 1, 1: 1}
+    label = 1
+    data_list = [[1, 1, 1], [1, 1, 1], [1, 2, 1], [2, 2, 1], [2, 2, 1],
                [2, 2, 0], [2, 3, 0], [2, 3, 0], [1, 1, 0], [3, 2, 0]]
-    rule_item = RuleItem(cond_set, class_label, dataset)
-    rule_item.print()
-    rule_item.print_rule()
-    print('condsupCount =', rule_item.cond_sup_count)   # should be 3
-    print('rulesupCount =', rule_item.rule_sup_count)   # should be 2
-    print('support =', rule_item.support)               # should be 0.2
-    print('confidence =', rule_item.confidence)         # should be 0.667
+    ruleitem = RuleItem(condset, label, data_list)
+    ruleitem.print_ruleitem()
+    ruleitem.print_rule()
+    print('condsupCount =', ruleitem.condsupCount)   # should be 3
+    print('rulesupCount =', ruleitem.rulesupCount)   # should be 2
+    print('support =', ruleitem.support)               # should be 0.2
+    print('confidence =', ruleitem.confidence)         # should be 0.667
